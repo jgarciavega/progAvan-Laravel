19e4b1b09ae08b23a5c2e668a4651798da0a6848
jgarciavega <99927975+jgarciavega@users.noreply.github.com>
2023-10-31 13:39:47 -0700

    Add files via upload
    Mi primer proyecto laravel
    lo hice manual no pude por consola

---
+++ storage/framework/views/60b82298091f805f736b6f5bef62b66e.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    HOLA MUNDO!!
+</body>
+</html><?php /**PATH C:\xampp\htdocs\proyectoLaravel\resources\views/app.blade.php ENDPATH**/ ?>
